f696b38f18928952d2aa86791112d3cfce257cce
Dennis Bundschuh <dennis.bundschuh@ancud.de>
2017-05-17 15:58:18 +0200

    added install-ubuntu.sh; cleanup

---
+++ install-ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+&& sudo apt-get update \
+&& sudo apt-get install -y \
+i3 \
+rxvt-unicode-256-color \
+nm-applet \
+nitrogen \
+compton \
+xfce4-power-manager \
+autocutsel \
+light-themes \
+ubuntu-gnome-desktop \
+gdm \
+pcmanfm \
+scrot \
+git \
+&& sudo apt-get remove -y unity
