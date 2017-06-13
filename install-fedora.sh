553da9a4addbab06ed06b67e719e43e598049ca1
Dennis Bundschuh <dennis.bundschuh@ancud.de>
2017-06-13 15:42:59 +0200

    added install script for fedora

---
+++ install-fedora.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+sudo dnf update \
+&& sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+&& sudo dnf install -y \
+i3 \
+rxvt-unicode-256color \
+nitrogen \
+compton \
+xfce4-power-manager \
+light-theme-gnome \
+pcmanfm \
+scrot \
+git \
+arandr \
+tlp \
+lxappearance \
+numix-gtk-theme \
+numix-icon-theme \
+numix-icon-theme-circle \
+vim
