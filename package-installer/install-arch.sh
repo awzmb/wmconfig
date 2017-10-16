9f66d01ef1958854d891d7e2312500939b55e7c4
Dennis Bundschuh <dennis.bundschuh@ancud.de>
2017-10-16 13:46:29 +0200

    added wallpapers

---
+++ package-installer/install-arch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sudo pacman -Sy \
+  file-roller \
+  jdk8-openjdk \
+  scrot \
+  firefox \
+  compton \
+  xfce4-settings \
+  lxterminal \
+  blueman \
+  docker \
+  docker-compose \
+  python-docker \
+  pcmanfm \
+  atom \
+  steam \
+  feh
