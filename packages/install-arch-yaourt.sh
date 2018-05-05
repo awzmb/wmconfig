56e16d0840c0fbd23d680ec1c5395ad4b337bbf9
Dennis Bundschuh <dennis.bundschuh@ancud.de>
2018-05-05 18:22:17 +0200

    cleaned up folders and added vim ide configuration

---
+++ packages/install-arch-yaourt.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+yaourt flashfocus &&
+yaourt spotify &&
+yaourt intellij &&
+yaourt adobe-source-code-pro-fonts &&
+yaourt hipchat &&
+yaourt ultra flat icons &&
+yaourt lxdm theme &&
+yaourt arc flatabulous &&
+yaourt vundle
+
+# YouCompleteMe workaround for ncurses5-lib-compat
+sudo pacman-key --refresh-keys
+gpg --keyserver pgp.mit.edu --recv-keys C52048C0C0748FEE227D47A2702353E0F7E48EDB &&
+yaourt ncurses5-compat-libs
