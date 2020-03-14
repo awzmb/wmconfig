b4d40acaa857abfb6f03bc547fdf7bf756207038
Dennis Bundschuh <bundschuh.dennis@gmail.com>
2020-03-14 03:45:26 +0100

    added dunst configuration

--- save-config.sh
+++ save-config.sh
@@ -2,7 +2,7 @@
 BASEPATH=./shell
 HOMEPATH=~/
 
-for application in "alacritty" "clipit" "i3" "mc" "nvim" "polybar" "vifm" "touchegg" "xfce4" "Code/User" "rofi"
+for application in "alacritty" "clipit" "i3" "mc" "nvim" "polybar" "vifm" "touchegg" "xfce4" "Code/User" "rofi" "dunst"
 do
   mkdir -p ${BASEPATH}/.config/${application}
   cp -r ${HOMEPATH}/.config/${application} ${BASEPATH}/.config/
