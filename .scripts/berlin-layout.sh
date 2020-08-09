1506c670b227d1f4844e376dbeeda1e65d58614b
Dennis Bundschuh <dennis.bundschuh@ancud.de>
2020-08-09 15:44:51 +0200

    added dotbot submodule

--- .scripts/berlin-layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xrandr \
---output DP1 --mode 1920x1080 --pos 0x0 --rotate normal \
---output eDP1 --primary --mode 1600x900 --pos 1920x0 --rotate normal &&
-nitrogen --restore
-~/.config/i3/polybar.sh
--- .scripts/bsp-layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xrandr \
---output LVDS1 --mode 1366x768 --pos 1920x0 --rotate normal \
---output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal &&
-nitrogen --restore
-~/.i3/polybar.sh
