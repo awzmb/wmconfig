d485632e2c3936ddc13323b8bca04aaa07cebebe
awzmb <bundschuh.dennis@gmail.com>
2021-12-29 20:43:02 +0100

    feat(debian): lightdm-lock and github cli

--- linux/install-debian.sh
+++ linux/install-debian.sh
@@ -226,6 +226,12 @@ sudo apt -y install steam
 # additional stuff
 unset $SSH_ASKPASS
 
+# githubcli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 # workaround: remove xdg-desktop-portal services to prevent slow
 # launch of some gtk apps (common problem with debian)
 sudo apt remove \
