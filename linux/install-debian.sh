5fd79222c53994313c9b6a3018e6554512783f86
awzmb <bundschuh.dennis@gmail.com>
2021-12-29 23:26:47 +0100

    feat(debian): discord theme, gtk2 settings and yarn

--- linux/install-debian.sh
+++ linux/install-debian.sh
@@ -74,6 +74,12 @@ sudo apt -y install \
   swaybar \
   xwayland
 
+# install js stuff (mainly for coc)
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt -y install yarn
+
 # install bluetooth applet
 sudo apt -y install \
   blueman
