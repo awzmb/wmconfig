a86895d498947986d5267855d9314dff3f538e8c
awzmb <bundschuh.dennis@gmail.com>
2024-01-22 17:04:58 -0500

    fix(fedora): rpm-fusion for ostree; libva; intel drivers

--- linux/fedora/install-fedora-rpm-ostree.sh
+++ linux/fedora/install-fedora-rpm-ostree.sh
@@ -3,14 +3,15 @@
 ORIGIN_PATH=${pwd}
 
 # enable rpmfusion repositories
-sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo rpm -Uvh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+rpm-ostree --apply-live install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # install non-free multimedia codecs
-sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf install rpmfusion-nonfree-release-tainted
-sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware"
-sudo dnf -y install intel-media-driver
+#sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
+#sudo dnf install rpmfusion-nonfree-release-tainted
+#sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware"
+#sudo dnf -y install intel-media-driver
 
 # layered packages
 sudo rpm-ostree -y --apply-live install \
@@ -63,12 +64,20 @@ sudo rpm-ostree -y --apply-live install \
     driverctl \
     wireguard-tools \
     kubernetes-client \
+    gnome-shell-extension-user-theme \
     npm \
     htop \
     distrobox \
     gtk-murrine-engine \
-    gtk2-engines
-
+    gtk2-engines \
+    ffmpeg-free \
+    gstreamer1-vaapi \
+    libvdpau-va-gl \
+    libva-utils \
+    libva-intel-driver \
+    libva-vdpau-driver \
+    vulkan-tools \
+    intel-media-driver
 
 # flathub repositories and premise
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -77,6 +86,7 @@ flatpak install --user flathub org.gnome.Sdk
 flatpak install --user flathub com.spotify.Client
 flatpak install --user flathub com.valvesoftware.Steam
 flatpak install --user flathub com.github.Eloston.UngoogledChromium
+flatpak install --user org.gtk.Gtk3theme.Qogir-dark
 flatpak install -y org.freedesktop.Platform.ffmpeg-full
 flatpak install -y org.freedesktop.Platform.GStreamer.gstreamer-vaapi
 
@@ -86,9 +96,17 @@ git clone https://github.com/vinceliuice/Qogir-theme.git ${HOME}/.themes/qogir-i
 ./${HOME}/.themes/qogir-install/install.sh
 rm -rf ${HOME}/.themes/qogir-install
 
-# give flatpak access to theme directories
+# give flatpak access to theme directories and set qogir theme
 sudo flatpak override --filesystem=$HOME/.themes
 sudo flatpak override --filesystem=$HOME/.icons
+sudo flatpak override --env=GTK_THEME=Qogir-Dark
+sudo flatpak override --env=ICON_THEME=Paper
+
+# firefox userchrome.css
+FIREFOX_PROFILE_DIRECTORY=$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed s/^Path=// | grep release)
+for profile in ${FIREFOX_PROFILE_DIRECTORY}; do
+  git clone https://github.com/awzmb/userchrome $profile/chrome
+done
 
 # install vim-plug plugin manager
 # for vim and neovim
@@ -103,86 +121,28 @@ sudo systemctl enable tlp
 # flashfocus
 sudo pip install flashfocus
 
-# python environment
-sudo dnf -y install \
-    pipenv \
-    python3-autopep8 \
-    python3-pandas \
-
-# java environment
-sudo dnf -y install \
-    java-1.8.0-openjdk
-
-# spotify terminal
-sudo dnf -y copr enable zeno/spotify-rust
-sudo dnf install -y spotifyd spotify-tui
-systemctl --user enable --now spotifyd.service
-
-# snap container platform
-#sudo dnf -y install snapd
-
-# flatpak container platform
-sudo dnf -y install flatpak
-
-# zathura document viewer
-sudo dnf install -y \
-    zathura \
-    zathura-pdf-mupdf
-
-# browser
-sudo dnf -y install \
-    chromium-browser-privacy \
-    firejail surf
-# vscodium
-sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf -y install codium
-
 # vulkan graphics
 sudo dnf -y install vulkan-loader vulkan-headers vulkan-tools
 
-# wine and dxvk
-sudo dnf -y install \
-    lutris \
-    wine \
-    wine-dxvk \
-    wine-dxvk-dxgi
-
-# hearthstone lutris / wine premise
-sudo dnf -y install \
-    gnutls \
-    gnutls-devel \
-    openldap \
-    openldap-devel \
-    libgpg-error \
-    sqlite2.i686 \
-    sqlite2.x86_64
-
-# email client
-sudo dnf -y copr enable flatcap/neomutt
-sudo dnf -y install \
-    neomutt \
-    notmuch \
-    isync \
-    msmtp
+#sudo dnf -y install \
+    #neomutt \
+    #notmuch \
+    #isync \
+    #msmtp
 
 # install mutt-wizard
-git clone https://github.com/LukeSmithxyz/mutt-wizard
-cd mutt-wizard
-sudo make install
-cd ${ORIGIN_PATH}
-rm -rf mutt-wizard
-
-# install themes from repository
-sudo dnf -y install \
-  gnome-shell-extension-user-theme \
+#git clone https://github.com/LukeSmithxyz/mutt-wizard
+#cd mutt-wizard
+#sudo make install
+#cd ${ORIGIN_PATH}
+#rm -rf mutt-wizard
 
 # aws tools
-sudo dnf -y install \
-    aws-tools \
-    awscli
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
+#sudo dnf -y install \
+    #aws-tools \
+    #awscli
+#curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+#sudo mv /tmp/eksctl /usr/local/bin
 
 # kubernetes and minikube
 #sudo dnf -y install \
@@ -200,58 +160,31 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 rm ./get_helm.sh
 
-# theme hospital foss
-# sudo dnf -y install corsix-th corsix-th-data
-
-# julius (caesarIII clone)
-# sudo dnf -y install julius
-
 # dwarf fortress
 # sudo dnf -y install dwarffortress
 
-# vim dependencies
-sudo dnf -y install \
-  yarnpkg
-
-# uninstall unnecessary packages
-#sudo dnf -y remove \
-  #xscreensaver \
-  #xscreensaver-base \
-  #abrt \
-  #tumbler \
-  #epiphany-runtime \
-  #mpv \
-  #tracker \
-  #tracker-miners \
-  #blueberry
-
 # install sway wayland wm
-sudo dnf -y install \
-  foot \
-  dmenu \
-  wofi \
-  fuzzel \
-  sway \
-  swaylock \
-  swayidle \
-  xwayland \
-  xorg-x11-server-Xwayland \
-  i3status \
-  i3status-config-fedora
-
-# kde plasma
-sudo dnf -y install \
-  @kde-desktop \
-  bismuth
+#sudo dnf -y install \
+  #foot \
+  #dmenu \
+  #wofi \
+  #fuzzel \
+  #sway \
+  #swaylock \
+  #swayidle \
+  #xwayland \
+  #xorg-x11-server-Xwayland \
+  #i3status \
+  #i3status-config-fedora
 
 # install gnome packages
-sudo dnf -y install \
-  gnome-tweaks \
-  gnome-extensions-app \
-  gnome-shell-extension-pop-shell \
-  gnome-shell-extension-pop-shell-shortcut-overrides \
-  gnome-shell-extension-unite \
-  xprop
+#sudo dnf -y install \
+  #gnome-tweaks \
+  #gnome-extensions-app \
+  #gnome-shell-extension-pop-shell \
+  #gnome-shell-extension-pop-shell-shortcut-overrides \
+  #gnome-shell-extension-unite \
+  #xprop
 
 # install xwayland standalone
 #sudo dnf copr enable ofourdan/Xwayland
@@ -267,13 +200,6 @@ sudo wget https://goo.gl/H2SViY -O /etc/udev/rules.d/99-8bitdo-bluetooth-control
 #sudo echo "/usr/bin/macchanger -r interface" >> /etc/netctl/interfaces/wlp2s0
 #sudo chmod +x /etc/netctl/interfaces/wlp2s0
 
-# install steam gaming platform
-sudo dnf -y install steam
-
-# parsec streaming service
-wget https://tinyurl.com/parsec-fedora ; bash parsec-fedora
-rm parsec-fedora
-
 # install snap packages
 #snap install \
     #adapta-gtk-snap \
@@ -283,83 +209,52 @@ rm parsec-fedora
     #spotifyd \
     #spt
 
-# install hsetroot for i3wm solid color background
-sudo dnf -y copr enable skidnik/hsetroot
-sudo dnf install hsetroot
-
-# install flashfocus for visual feedback
-# on windows switch
+# install flashfocus for visual feedback on windows switch
 sudo pip install flashfocus
 
 # unclutter (hides mouse when idle)
-sudo dnf -y install unclutter
+#sudo dnf -y install unclutter
 
 # install spotify-tui
-sudo dnf -y copr enable szpadel/spotifyd
-sudo dnf -y copr enable atim/spotify-tui
-sudo dnf -y install \
-    spotifyd \
-    spotify-tui
-
-# install flatpak packages
-#snap install \
-    #com.discordapp.Discord \
-    #com.spotify.Client \
-    #com.teamspeak.TeamSpeak \
-    #org.gtk.Gtk3theme.Qogir \
-    #org.gtk.Gtk3theme.Qogir-win-dark \
-    #net.sourceforge.chromium-bsu
+#sudo dnf -y copr enable szpadel/spotifyd
+#sudo dnf -y copr enable atim/spotify-tui
+#sudo dnf -y install \
+    #spotifyd \
+    #spotify-tui
 
 # install asdf for tool management
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf
-git checkout "$(git describe --abbrev=0 --tags)"
+#git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+#cd ~/.asdf
+#git checkout "$(git describe --abbrev=0 --tags)"
 
 # change lightdm background
-sed -i 's/^background=.*/background=#242933/g' /etc/lightdm/lightdm-gtk-greeter.conf
+#sed -i 's/^background=.*/background=#242933/g' /etc/lightdm/lightdm-gtk-greeter.conf
 
 # change timezone to europe/berlin
-sudo rm -rf /etc/localtime
-sudo ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+#sudo rm -rf /etc/localtime
+#sudo ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
 # change grub theme
-sudo mkdir -p /boot/grub/themes/fedora
-sudo cp ${PWD}/grub/theme.txt /boot/grub/themes/fedora/theme.txt
-sudo sed -i "\$aGRUB_THEME=/boot/grub/themes/fedora/theme.txt" /etc/default/grub
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-
-# install iris drivers and libva
-sudo dnf -y install \
-  libva-utils \
-  libva-intel-driver \
-  libva-intel-hybrid-driver \
-  libvdpau \
-  libva-utils \
-  libva-vdpau-driver \
-  intel-media-driver \
-  libvdpau-va-gl \
-  ffmpeg
+#sudo mkdir -p /boot/grub/themes/fedora
+#sudo cp ${PWD}/grub/theme.txt /boot/grub/themes/fedora/theme.txt
+#sudo sed -i "\$aGRUB_THEME=/boot/grub/themes/fedora/theme.txt" /etc/default/grub
+#sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # set liva driver to iHD for vainfo to work
-if [ ! -n "$(cat /etc/environment | grep LIBVA_DRIVER_NAME)" ]; then
-  echo 'LIBVA_DRIVER_NAME=iHD' | sudo tee -a /etc/environment
-fi
+#if [ ! -n "$(cat /etc/environment | grep LIBVA_DRIVER_NAME)" ]; then
+  #echo 'LIBVA_DRIVER_NAME=iHD' | sudo tee -a /etc/environment
+#fi
 
-sudo groupadd shadow-input
-sudo usermod -a -G input $USER
-sudo usermod -a -G shadow-input $USER
-echo "uinput" | sudo tee -a /etc/modules-load.d/uinput.conf
-echo 'KERNEL=="uinput", MODE="0660", GROUP="shadow-input"' | sudo tee -a /etc/udev/rules.d/65-shadow-client.rules
-
-# provide vulkan and metal support
-sudo dnf -y install \
-  vulkan \
-  vulkan-tools
+#sudo groupadd shadow-input
+#sudo usermod -a -G input $USER
+#sudo usermod -a -G shadow-input $USER
+#echo "uinput" | sudo tee -a /etc/modules-load.d/uinput.conf
+#echo 'KERNEL=="uinput", MODE="0660", GROUP="shadow-input"' | sudo tee -a /etc/udev/rules.d/65-shadow-client.rules
 
 # add options to i915 to enable libva / vainfo
-if [ ! -f "/etc/modprobe.d/i915.conf" ]; then
-  echo "options i915 enable_guc=2" | sudo tee -a /etc/modprobe.d/i915.conf
-fi
+#if [ ! -f "/etc/modprobe.d/i915.conf" ]; then
+  #echo "options i915 enable_guc=2" | sudo tee -a /etc/modprobe.d/i915.conf
+#fi
 
 # gnome shell settings
 # solid color background
@@ -369,28 +264,13 @@ gsettings set org.gnome.desktop.background color-shading-type 'solid'
 # disable extension validation
 gsettings set org.gnome.shell disable-extension-version-validation true
 # set pop shell keybinds
-./pop-shell/pop-shell-keybinds.sh
-
-# firefox configuration
-FIREFOX_PROFILE_DIRECTORY=$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed s/^Path=// | grep release)
-for profile in ${FIREFOX_PROFILE_DIRECTORY}; do
-  git clone https://github.com/awzmb/userchrome $profile/chrome
-done
-
-# install citrix workspace
-# git clone https://github.com/dcloud-ca/ca.dcloud.ICAClient ~/workspace/flathub-citrix-reciever
-# cd ~/workspace/flathub-citrix-reciever
-# flatpak-builder --user --install --force-clean icaclient ca.dcloud.ICAClient.yml
+#./pop-shell/pop-shell-keybinds.sh
 
 # password management
 #sudo dnf -y install gnupg
 #gpg --full-gen-key && \
 #pass init bundschuh.dennis@gmail.com \
 #pass insert mail/main
-
-# librewolf (custom firefox)
-sudo dnf config-manager --add-repo https://rpm.librewolf.net/librewolf-repo.repo
-sudo dnf -y install librewolf
 
 # additional stuff
 unset $SSH_ASKPASS
