e10d7d4229ebb6a3980d10c75553977f6832b84a
awzmb <bundschuh.dennis@gmail.com>
2025-02-08 12:17:09 +0100

    feat(fedora): fixed qogir theme installer

--- linux/fedora/install-fedora-rpm-ostree.sh
+++ linux/fedora/install-fedora-rpm-ostree.sh
@@ -252,7 +252,7 @@ flatpak override --user --filesystem=~/.mozilla org.mozilla.firefox
 # qogir theme
 mkdir -p ${HOME}/.themes
 git clone https://github.com/vinceliuice/Qogir-theme.git ${HOME}/.themes/qogir-install
-./${HOME}/.themes/qogir-install/install.sh
+${HOME}/.themes/qogir-install/install.sh
 rm -rf ${HOME}/.themes/qogir-install
 
 # flatcolor theme (base16)
