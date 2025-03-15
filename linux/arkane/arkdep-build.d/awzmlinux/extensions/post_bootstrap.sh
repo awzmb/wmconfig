f87fd230a796fb51dc2a3d146d45f9eeeef0c6f9
Dennis Bundschuh <dbundschuh@it4ipm.de>
2025-03-15 11:07:34 +0100

    fix(arkane): removed depedency build; fixed workaround step; fixed intel bt problems

---
+++ linux/arkane/arkdep-build.d/awzmlinux/extensions/post_bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# reference custom sway startup script in systemd unit
+sed -i -e 's|Exec=.*|Exec=/usr/local/bin/sway-egpu|g' /usr/share/wayland-sessions/sway.desktop
+
+# remove fallback hyprland configuration
+rm -f /usr/share/hypr/hyprland.conf
+
+# workaround for https://www.reddit.com/r/archlinux/comments/1ja6y69/it_looks_like_linuxfirmware_20250311b69d4b742_has/
+TMP_DIR=$(mktemp -d)
+curl -L https://archive.archlinux.org/packages/l/linux-firmware/linux-firmware-20250210.5bc5868b-1-any.pkg.tar.zst -o $TMP_DIR/linux-firmware-20250210.5bc5868b-1-any.pkg.tar.zst
+pacman -U --noconfirm $TMP_DIR/linux-firmware-20250210.5bc5868b-1-any.pkg.tar.zst
+rm -rf $TMP_DIR
--- linux/arkane/arkdep-build.d/extensions/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# reference custom sway startup script in systemd unit
-sed -i -e 's|Exec=.*|Exec=/usr/local/bin/sway-egpu|g' /usr/share/wayland-sessions/sway.desktop
-
-# remove fallback hyprland configuration
-rm -f /usr/share/hypr/hyprland.conf
