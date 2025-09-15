bd9a26f40375ee487178327394b465999a4261cf
Dennis Bundschuh <dbundschuh@it4ipm.de>
2025-09-15 02:50:06 +0200

    feat(arkane): added yay to install hy3

--- linux/arkane/arkdep-build.d/awzmlinux/extensions/post_install.sh
+++ linux/arkane/arkdep-build.d/awzmlinux/extensions/post_install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # reference custom sway startup script in systemd unit
-sed -i -e 's|Exec=.*|Exec=/usr/local/bin/sway-egpu|g' /usr/share/wayland-sessions/sway.desktop
+arch-chroot "$workdir" sed -i -e 's|Exec=.*|Exec=/usr/local/bin/sway-egpu|g' /usr/share/wayland-sessions/sway.desktop
 
 # remove fallback hyprland configuration
-rm -f /usr/share/hypr/hyprland.conf
+arch-chroot "$workdir" rm -f /usr/share/hypr/hyprland.conf
 
 # workaround for https://www.reddit.com/r/archlinux/comments/1ja6y69/it_looks_like_linuxfirmware_20250311b69d4b742_has/
 #TMP_DIR=$(mktemp -d)
@@ -13,8 +13,43 @@ rm -f /usr/share/hypr/hyprland.conf
 #rm -rf $TMP_DIR
 
 # set hostname
-hostnamectl set-hostname 'L0223-1024'
+arch-chroot "$workdir" hostnamectl set-hostname 'L0223-1024'
 
-# enable seatd and keyd
-systemctl enable seatd
-systemctl enable keyd
+# Install AUR packages
+# Set list of AUR packages to install
+aur_packages=('yay-bin' 'paru-bin' 'hy3')
+
+# Install build dependencies
+printf '\e[1;32m-->\e[0m\e[1m Installing build dependencies\e[0m\n'
+arch-chroot "$workdir" pacman -Sy --noconfirm --needed base-devel git
+
+# Create temporary unprivileged user, required for fakeroot
+printf '\e[1;32m-->\e[0m\e[1m Creating temporary user\e[0m\n'
+arch-chroot "$workdir" useradd aur -m -p '!'
+
+# Allow 'aur' to use sudo without password
+printf '\e[1;32m-->\e[0m\e[1m Allowing aur user passwordless sudo\e[0m\n'
+arch-chroot "$workdir" bash -c "echo 'aur ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/aur"
+
+# Install yay manually first (because we need it to install others)
+printf '\e[1;32m-->\e[0m\e[1m Bootstrapping yay-bin\e[0m\n'
+arch-chroot -u aur:aur "$workdir" bash -c "
+  cd /home/aur &&
+  git clone https://aur.archlinux.org/yay-bin.git &&
+  cd yay-bin &&
+  makepkg -si --noconfirm
+"
+
+# Install AUR packages using yay
+for package in "${aur_packages[@]}"; do
+    printf "\e[1;32m-->\e[0m\e[1m Installing $package using yay\e[0m\n"
+    arch-chroot -u aur:aur "$workdir" bash -c "yay -S --noconfirm $package"
+done
+
+# Cleanup sudoers file
+printf '\e[1;32m-->\e[0m\e[1m Removing temporary sudoers rule\e[0m\n'
+arch-chroot "$workdir" rm -f /etc/sudoers.d/aur
+
+# Cleanup user
+printf '\e[1;32m-->\e[0m\e[1m Performing cleanup\e[0m\n'
+arch-chroot "$workdir" userdel -r aur
