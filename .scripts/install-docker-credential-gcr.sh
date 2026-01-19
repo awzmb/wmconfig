ad7659ae4c55bff4c55a2c42093219c701ef8c71
Dennis Bundschuh <dbundschuh@it4ipm.de>
2026-01-19 17:33:42 +0100

    fix(sway): lid management not working on secondary laptop

--- .scripts/install-docker-credential-gcr.sh
+++ .scripts/install-docker-credential-gcr.sh
@@ -13,7 +13,7 @@ case ${ARCH} in
     ARCH=arm64
     ;;
   amd64|x86_64)
-    ARCH=x86_64
+    ARCH=amd64
     ;;
   *)
     fatal "Unsupported architecture ${ARCH}"
@@ -23,10 +23,10 @@ esac
 mkdir -p ${INSTALL_DIR}
 
 #DOCKER_CREDENTIAL_GCR_VERSION=$(curl -s https://api.github.com/repos/googlecloudplatform/docker-credential-gcr/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1')
-DOCKER_CREDENTIAL_GCR_VERSION=2.1.24
-DOCKER_CREDENTIAL_GCR_ARCHIVE=" docker-credential-gcr_$(uname -s | tr '[:upper:]' '[:lower:]')_${ARCH}-${DOCKER_CREDENTIAL_GCR_VERSION}.tar.gz"
+DOCKER_CREDENTIAL_GCR_VERSION=2.1.31
+DOCKER_CREDENTIAL_GCR_ARCHIVE="docker-credential-gcr_$(uname -s | tr '[:upper:]' '[:lower:]')_${ARCH}-${DOCKER_CREDENTIAL_GCR_VERSION}.tar.gz"
 curl -L --output "${TMP_DIR}/${DOCKER_CREDENTIAL_GCR_ARCHIVE}" --url "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${DOCKER_CREDENTIAL_GCR_VERSION}/${DOCKER_CREDENTIAL_GCR_ARCHIVE}"
 tar xfz ${TMP_DIR}/${DOCKER_CREDENTIAL_GCR_ARCHIVE} -C ${TMP_DIR}
-mv ${TMP_DIR}/docker-credential-gcloud ${INSTALL_DIR}
+mv ${TMP_DIR}/docker-credential-gcr ${INSTALL_DIR}/docker-credential-gcloud
 chmod +x ${INSTALL_DIR}/docker-credential-gcloud
 echo $(which docker-credential-gcloud)
