34ad6cd265fffe2a6955ec694bcb69a7ee54c3c0
Dennis Bundschuh <dennis.bundschuh@gema.de>
2026-07-24 08:06:45 +0200

    feat(fedora-bootc): run neovim container unconfined to make register->host clipboard work

--- .scripts/sway-home-display-setup.sh
+++ .scripts/sway-home-display-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 swaymsg 'output "QHX GF340H Unknown" transform 90 mode 2560x1080@60Hz position 0 0'
 swaymsg 'output "Samsung Electric Company LS49AG95 HNTW800039" mode 3840x1080@120Hz position 2560 0'
-swaymsg 'output "BOE YHB0AP23 Unknown" transform 90 position 6400 0'
+swaymsg 'output "BOE YHB0AP23 Unknown" transform 270 position 6400 0'
